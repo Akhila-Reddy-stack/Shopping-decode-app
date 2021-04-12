0cd85309e204aa31a5e11769cf51c605bc415225
Akhila-Reddy-stack <60685579+Akhila-Reddy-stack@users.noreply.github.com>
2021-04-12 10:21:25 -0800

    Add files via upload

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -8,42 +8,68 @@ import Modal from 'react-responsive-modal';
 import * as React from 'react';
 import _ from 'lodash';
 import * as InoIcons from 'react-icons/io';
-// import '../../styles/nav.scss';
-import SideNav from '../NavBar/Sidebar';
-import SignUp from '../Auth/SignUp';
+import '../../styles/nav.scss'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PersonIcon from '@material-ui/icons/Person';
+import HeadsetMicIcon from '@material-ui/icons/HeadsetMic';
+import RoomIcon from '@material-ui/icons/Room';
+import SideNav from '../NavBar/Sidebar'
+import SignUp from '../Auth/SignUp'
 import { Link } from 'react-router-dom';
 
 class NavBar extends PureComponent {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: {},
-			loggedIn: false,
-			open: false,
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {},
+            loggedIn: false,
+            open: false
+        };
+    }
 
-	setFormApi = (formApi) => {
-		this.formApi = formApi;
-	};
+    setFormApi = (formApi) => {
+        this.formApi = formApi
+    }
 
-	componentDidMount = async () => {};
 
-	userSignIn = async () => {
-		await this.setState({
-			open: true,
-		});
-	};
+    componentDidMount = async () => {
+    }
 
-	render() {
-		const { loggedIn, open } = this.state;
 
-		return (
-			<Fragment>
-				<ReactNotification ref={this.notificationDOMRef} />
-				<SideNav className="nav-tog" />
-			</Fragment>
-		);
-	}
+
+    userSignIn = async () => {
+        await this.setState({
+            open: true
+        })
+    }
+
+
+    render() {
+
+        const { loggedIn ,open} = this.state;
+
+
+        return (
+            <Fragment>
+                   
+                <ReactNotification ref={this.notificationDOMRef} />
+                <SideNav className="nav-tog" />
+
+            </Fragment>
+
+
+
+        )
+    }
+
 }
-export default NavBar;
+export default (NavBar)
+
+
+
+
+
+
+
+
+
